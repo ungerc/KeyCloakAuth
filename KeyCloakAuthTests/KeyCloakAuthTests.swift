3bd9c9b4c6a1ecb4ac7a73a991f278615471f328
cunger <christian.unger@icloud.com>
2025-06-08 13:43:59 +0200

    Initial Commit

---
+++ KeyCloakAuthTests/KeyCloakAuthTests.swift
@@ -0,0 +1,17 @@
+//
+//  KeyCloakAuthTests.swift
+//  KeyCloakAuthTests
+//
+//  Created by Christian Unger on 08.06.25.
+//
+
+import Testing
+@testable import KeyCloakAuth
+
+struct KeyCloakAuthTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
